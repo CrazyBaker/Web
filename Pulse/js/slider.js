9d404c436e41ddffb11a6185c723be58305f527e
Alexey <zigunovchrome@gmail.com>
2022-04-30 23:55:32 +0300

    add slider section

---
+++ Pulse/js/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left-arrow.png" alt="left-arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right-arrow.png" alt="right-arrow"></button>'
+    });
+});
+
+
+
+
+
